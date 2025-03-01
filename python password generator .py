98f8ca9b746d510ec138a0e34d06a1d6b5a420b7
Femboy <skibidi@furaffinity.org>
2025-03-01 18:06:33 +0000

    rizz works now

--- python password generator .py
+++ python password generator .py
@@ -1,23 +1,13 @@
 import random
 
-num=2
+def generate_password(pl):
+	pw = ""
+	pwc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()?"
+	for i in range(pl):
+		pw += random.choice(pwc)
+	return pw
 
-while num>0:
-   def generate_password():
-     password_length = 15 # Set the       length of the password here
-
-  # Create a string of all the possible characters that can go into the password
-     password_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()?"
-
-  # Initialize the password as an empty string
-     password = ""
-
-  # Choose random characters and add them to the password until it reaches the desired length
-     for i in range(password_length):
-       password += random.   choice(password_characters)
-
-     return password
-
-# Generate a password and print it to the screen
-   print(generate_password())
-   num=num-1
+pl = int(input("Enter the length of the password: "))
+pw = generate_password(pl)
+print(pw)
+input("Press Enter to exit")
